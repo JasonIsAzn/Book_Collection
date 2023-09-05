11d02452fba54529de50050b0621ee3fd02349ad
jason <jasonisazn@tamu.edu>
2023-09-05 05:02:01 +0000

    added styling to home screen

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll(".myButtonClass");
+    buttons.forEach(function(button) {
+      button.addEventListener("click", myFunction);
+    });
+  });
+
+  
+function myFunction() {
+  alert("No Implementation");
+}
