7f3697721c1a564dd94e3381d7a3d89eeb67f6ee
Jason <lejason123@gmail.com>
2023-09-13 00:11:42 -0500

    add user books

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_many :book, through: :user_books
+  has_many :user_books
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'books#index' 
+  resources :user_books
+  resources :users
+  root 'books#index'
   resources :books do
     member do
       get :delete
     end
   end
-
 end
